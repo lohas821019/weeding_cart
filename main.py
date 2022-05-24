37e4affc2062abb05c4ec22350d4f8a89a80ec0f
JasonHuang <lohas821019@gmail.com>
2022-05-24 15:57:44 +0800

    增加region 用來存所抓取到的region

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def worker():
     while True:
         mid = weed_signal.get()
 
-        #重複移動讓手臂到定點
+        #迴圈重複判斷讓手臂到定點，是否到定點由camera產生的arm_loc 看他有沒有落在weed圈選的範圍
         try:
             if a[0] > 10 :
                 a[0] = 10
@@ -70,30 +70,37 @@ def worker():
                 a[1] = a[1]-1
                 weed_signal.put(a)
             arm_move(a)
-
         except:
             pass
-        
-        #如果手臂已經除完草，讓車移動
-        if mid == arm_loc:
+
+        #如果手臂已經到達定點，伸長手臂除草，然後讓車移動
+        if region[0] <= arm_loc[0] <=region[2] and region[1] <= arm_loc[1] <=region[3]:
+            #這邊再加入手臂伸長的動作
+            time.sleep(2)
+            #這邊加入手臂收回的動作
             car_signal.put()== 'move'
 
 #無人車行進設定
 def car_moving(s):
+    #如果quene收到'stop' 則停止，如果quene收到'move'，則車子移動
     if car_signal.get() == 'stop':
         motor_control(s,0)
     elif car_signal.get()== 'move':
         motor_control(s,1)
     else:
         pass
-    
+
+#手臂初始化
 # ans = arm_init()
+
+#Queue初始化宣告
 weed_signal = queue.Queue()
 car_signal = queue.Queue()
 
 threading.Thread(target=worker, daemon=True).start()
 threading.Thread(target=car_moving,args=(s,), daemon=True).start()
 #%%
+
 cap = cv2.VideoCapture(1)
 hw = []
 global arm_loc
@@ -108,33 +115,38 @@ while cap.isOpened():
         mid_px =(1.5*h)/2
         mid_py = w/2
         mid_pic = (mid_px,mid_py)
+        region = []
 
     roi = frame[int(h/2):h,0:w]
     results_roi = model(roi, size=640)  # includes NMS
     results_roi.pred
-    data = results_roi.pandas().xyxy[0]    # includes NMS
+    data = results_roi.pandas().xyxy[0]
     
-    imgColor_r,mask_r = myColorFinder.update(frame,hsvVals_r)
-    imgColor_g,mask_g = myColorFinder.update(frame,hsvVals_g)
-
-    imgContour_r,contours_r = cvzone.findContours(frame, mask_r)
-    imgContour_g,contours_g = cvzone.findContours(frame, mask_g)
+    #分辨出紅色與綠色
+    imgColor_r,mask_r = myColorFinder.update(roi,hsvVals_r)
+    imgColor_g,mask_g = myColorFinder.update(roi,hsvVals_g)
+    
+    #抓取出區域輪廓以及中心點
+    imgContour_r,contours_r = cvzone.findContours(roi, mask_r)
+    imgContour_g,contours_g = cvzone.findContours(roi, mask_g)
     imgStack_all = cvzone.stackImages([imgColor_r, imgColor_g, imgContour_r, imgContour_g],2,0.5)
     
     #找到紅色的區域(手臂的位置)
     if contours_r:
         arm_loc = contours_r[0]['center']
 
+    #將有判斷出來的雜草圈出，並且計算出中心點mid，並且用圓圈標出中心點
     try:
         for i in range(0,len(data)):
             data = data.iloc[i]
+            region = [int(data.xmin), int(data.ymin), int(data.xmax), int(data.ymax)]
             cv2.rectangle(roi, (int(data.xmin), int(data.ymin)), (int(data.xmax), int(data.ymax)), (0, 0, 255), 2)
             mid = ((data.xmin + data.xmax)/2,(data.ymin + data.ymax)/2)
-            print(mid)
-            cv2.circle(roi,(int(mid[0]),int(mid[1])), 15, (0, 0, 255), -1)
+            
+            # print(mid)
+            cv2.circle(roi,(int(mid[0]),int(mid[1])), 8, (0, 0, 255), -1)
             car_signal.put('stop')
             weed_signal.put(mid)
-
     except:
         pass
 
